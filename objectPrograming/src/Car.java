450596f9ff90223323edad1050e49ebafa728d00
Michał <michal.lukaszewski3@gmail.com>
2021-11-20 13:40:45 +0100

    Drugi Commit

---
+++ objectPrograming/src/Car.java
@@ -0,0 +1,142 @@
+//public class Car {
+//
+//    private int speed = 0;
+//    private int gear;
+//    private String name;
+//    private boolean lights;
+//
+//    public Car(int speed, int gear, String name, boolean lights);{
+//        this.speed = speed;
+//        this.gear = gear;
+//        this.name = name;
+//        this.lights = lights;
+//    }
+//
+//
+//
+//    public void startEngine() {
+//        System.out.println("Silnik działa");
+//    }
+//        public void stopEngine(){
+//            System.out.println("Silnik zgasł");
+//        }
+//
+//        public void accelerate (int accelerate){
+//        this.speed += accelerate;
+//        System.out.println("Samochód jedzie z prędkością "+speed +" km/h");
+//        }
+//        public void slowDown(int speed){
+//        this.speed -= speed;
+//        System.out.println("Samochód zwalnia do prędkości "+this.speed +" km/h");
+//        }
+//        public void setGear(int gear) {
+//            if (gear < 7 && gear > 0) {
+//                this.gear = gear;
+//                System.out.println("Jesteś na biegu " + this.gear);
+//            }
+//        }
+//        public void setLights(boolean lights) {
+//            this.lights = lights;
+//            }
+//        public boolean getLights(){
+//            return lights;
+//
+//            }
+//
+//    public int getSpeed() {
+//        return speed;
+//    }
+//
+//    public void setSpeed(int speed) {
+//        this.speed = speed;
+//    }
+//
+//    public int getGear() {
+//        return gear;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public boolean isLights() {
+//        return lights;
+//    }
+//}
+
+public class Car {
+
+    private int speed = 0;
+    private int gear;
+    private String model;
+    private boolean lights;
+
+    public Car(int speed, int gear, String model, boolean lights){
+        this.speed = speed;
+        this.gear = gear;
+        this.model = model;
+        this.lights = lights;
+    }
+
+    public Car(){
+    }
+    public Car(int speed, int gear, String model){
+        this.speed = speed;
+        this.gear = gear;
+        this.model = model;
+    }
+    public void startEngine() {
+        System.out.println("silnik dziala");
+    }
+    public void stopEngine(){
+        System.out.println("silnik zgasl");
+    }
+    public void accelerate(int acceleration){
+        this.speed += acceleration;
+        System.out.println("samochod jedzie z predkoscia " + speed+ "km/h");
+    }
+    public void slowDown(int speed){
+        this.speed -= speed;
+        System.out.println("samochod zwalnia do predkosci " + this.speed + "km/h");
+    }
+    public void setGear(int gear){
+        if(gear < 7 && gear > 0){
+            this.gear = gear;
+            System.out.println("jestes na biegu " + this.gear);
+        }
+    }
+    public void setLights(boolean lights){
+        this.lights = lights;
+    }
+    public boolean getLights(){
+        return lights;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getGear() {
+        return gear;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public boolean isLights() {
+        return lights;
+    }
+}
--- objectPrograming/src/Main.java
+++ objectPrograming/src/Main.java
@@ -1,5 +1,50 @@
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        Car audi = new Car(100, 2, "A6",true);
+//        audi.startEngine();
+//        audi.setLights(true);
+//        System.out.println(audi.getLights());
+//        audi.setGear(1);
+//        audi.accelerate(20)  ;
+//        audi.accelerate(20);
+//        audi.slowDown(10);
+//        audi.stopEngine();
+//
+//    }
+//}
+
 public class Main {
     public static void main(String[] args) {
 
+        Car audi = new Car(0, 1, "a3", true);
+
+        audi.startEngine();
+        audi.setLights(true);
+        System.out.println(audi.getLights());
+        audi.setGear(1);
+        audi.accelerate(20);
+        audi.accelerate(20);
+        audi.slowDown(10);
+        audi.stopEngine();
+
+        System.out.println("zmiana samochodu");
+
+        Car bmw = new Car(30, 3, "e36", false);
+        bmw.startEngine();
+        bmw.setLights(true);
+        System.out.println(bmw.getLights());
+        bmw.setGear(3);
+        bmw.accelerate(20);
+        bmw.accelerate(20);
+        bmw.slowDown(10);
+        bmw.stopEngine();
+
+        Car opel = new Car();
+        opel.startEngine();
+
+        Car volvo = new Car(10, 1, "jakis");
+        System.out.println(volvo.getModel());
+
     }
-}
+    }
